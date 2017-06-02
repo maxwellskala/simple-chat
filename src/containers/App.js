f91ce5017f03c5fb286801cbc93bfa5d34b5c0da
Max Skala <max.skala@odysys.com>
2017-06-01 20:37:27 -0400

    Clear message on send

--- src/containers/App.js
+++ src/containers/App.js
@@ -35,6 +35,7 @@ class App extends React.Component {
       },
       (status, response) => console.log(status, response)
     );
+    this.setState({ currentMessage: '' });
   };
 
   handleMessageChange(e) {
