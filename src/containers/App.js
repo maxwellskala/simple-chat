2c458b1ba003c8f73d8c24550d49a98153a785f9
Max Skala <max.skala@odysys.com>
2017-06-01 20:33:57 -0400

    Add basic PubNub send/receive

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,36 +1,83 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const CHANNEL = 'chat';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      message: []
+      currentMessage: '',
+      messages: []
     };
 
-    this.success = this.success.bind(this);
+    this.handleMessageReceive = this.handleMessageReceive.bind(this);
+    this.handleMessageChange = this.handleMessageChange.bind(this);
+    this.handleMessageSend = this.handleMessageSend.bind(this);
+    this.renderMessages = this.renderMessages.bind(this);
   };
 
-  success(m) {
+  handleMessageReceive(m) {
     console.log('receiving PubNub message');
+    const text = m.message;
     this.setState(prevState => ({
-      message: prevState.concat([m])
+      messages: prevState.messages.concat([text])
     }));
+  };
+
+  handleMessageSend(e) {
+    const { pubNub } = this.props;
+    const { currentMessage } = this.state;
+    pubNub.publish(
+      {
+        channel: CHANNEL,
+        message: currentMessage
+      },
+      (status, response) => console.log(status, response)
+    );
+  };
+
+  handleMessageChange(e) {
+    const message = e.target.value;
+    this.setState(prevState => ({
+      currentMessage: message
+    }));
+  };
+
+  renderMessages() {
+    return this.state.messages.map(message => (
+      <span key={message}>{message}</span>
+    ));
   };
 
   componentWillMount() {
     const { pubNub } = this.props;
     pubNub.addListener({
-      message: (m) => this.success(m)
+      message: this.handleMessageReceive
     });
     pubNub.subscribe({
-      channels: ['chat']
+      channels: [CHANNEL]
     });
   };
 
   render() {
-    return <div>Hello world! You are logged in as {this.props.username}.</div>;
+    const { currentMessage } = this.state;
+    return (
+      <div>
+        <h3>You are logged in as {this.props.username}.</h3>
+        {this.renderMessages()}
+        <label>
+          Message:
+          <input
+            type="text"
+            value={currentMessage}
+            onChange={this.handleMessageChange}
+          />
+          <button onClick={this.handleMessageSend}>Send</button>
+        </label>
+      </div>
+    );
   };
 };
 
