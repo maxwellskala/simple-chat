be45f474663b7b194ee7ee92fdb7f03465567fb8
Max Skala <max.skala@odysys.com>
2017-06-05 17:03:25 -0400

    Install testing packages and mock out tests for App

---
+++ src/tests/App.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import { wrap, wrapAndSetState } from './utils';
+
+function emptyFunc() {
+  return null;
+}
+
+const PUBNUB = {
+  addListener: emptyFunc,
+  subscribe: emptyFunc,
+  history: emptyFunc,
+  publish: emptyFunc,
+  setUUID: emptyFunc
+};
+
+describe('<App />', () => {
+  xit('renders user choice selection if no user in state', () => {
+    return;
+  });
+
+  xit('updates user state on click of user choice', () => {
+    return;
+  });
+
+  xit('renders loading message if state.historyFetched === false', () => {
+    return;
+  });
+
+  xit('renders one Conversation if state.user and state.historyFetched', () => {
+    return;
+  });
+
+  xit('renders on NewMessageInput if state.user and state.historyFetched', () => {
+
+  });
+
+  xit('properly updates state.currentMessage when NewMessageInput is altered', () => {
+
+  });
+
+  xit('resets state.currentText to empty string when message is sent', () => {
+
+  });
+});
---
+++ src/tests/utils.js
@@ -0,0 +1,20 @@
+import { shallow } from 'enzyme';
+
+export function wrap(component) {
+  return shallow(component);
+};
+
+export function wrapAndRender(component) {
+  return wrap(component).render();
+};
+
+export function wrapAndSetState(component, state) {
+  const wrapper = wrap(component);
+  wrapper.setState(state);
+  return wrapper;
+};
+
+export function setStateAndRender(component, state) {
+  const statefulWrapper = wrap(component).setState(state);
+  return statefulWrapper.render();
+};
