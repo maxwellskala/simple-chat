e57bd2ea005fbda75fc25de9bb74877059fbcd0c
Max Skala <max.skala@odysys.com>
2017-06-05 17:55:24 -0400

    Finish tests for App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import App from '../containers/App';
+import Conversation from '../components/Conversation';
+import NewMessageInput from '../components/NewMessageInput';
 import { wrap, wrapAndSetState } from './utils';
+import * as Usernames from '../constants/Usernames';
 
 function emptyFunc() {
   return null;
 }
 
-const PUBNUB = {
+const pubNub = {
   addListener: emptyFunc,
   subscribe: emptyFunc,
   history: emptyFunc,
@@ -14,32 +18,66 @@ const PUBNUB = {
   setUUID: emptyFunc
 };
 
+const DEFAULT_PROPS = { pubNub };
+
 describe('<App />', () => {
-  xit('renders user choice selection if no user in state', () => {
-    return;
+  it('renders user choice selection if no user in state', () => {
+    const wrapped = wrap(<App {...DEFAULT_PROPS} />);
+    expect(wrapped.find('button').length).toEqual(2);
+    expect(wrapped.find('h3').text()).toEqual('Choose your user');
   });
 
-  xit('updates user state on click of user choice', () => {
-    return;
+  it('updates user state on click of user choice', () => {
+    const wrapped = wrap(<App {...DEFAULT_PROPS} />);
+    expect(wrapped.state('user')).toEqual(null);
+    const nasButton = wrapped.findWhere(child => (
+      child.type() === 'button' && child.text() === 'Nas'
+    ));
+    nasButton.simulate('click');
+    expect(wrapped.state('user')).toEqual(Usernames.NAS);
   });
 
-  xit('renders loading message if state.historyFetched === false', () => {
-    return;
+  it('renders loading message if state.user && state.historyFetched === false', () => {
+    const wrapped = wrap(<App {...DEFAULT_PROPS} />);
+    const expectedNasText = 'Welcome back Nasir Jones. Fetching your chat history...';
+    expect(wrapped.findWhere(child => child.text() === expectedNasText).length)
+      .toEqual(0);
+    wrapped.setState({ user: Usernames.NAS });
+    expect(wrapped.findWhere(child => child.text() === expectedNasText).length)
+      .toEqual(1);
   });
 
-  xit('renders one Conversation if state.user and state.historyFetched', () => {
-    return;
+  it('renders one Conversation if state.user and state.historyFetched', () => {
+    const wrapped = wrap(<App {...DEFAULT_PROPS} />);
+    expect(wrapped.find(Conversation).length).toEqual(0);
+    wrapped.setState({ user: Usernames.NAS, historyFetched: true });
+    expect(wrapped.find(Conversation).length).toEqual(1);
   });
 
-  xit('renders on NewMessageInput if state.user and state.historyFetched', () => {
-    return;
+  it('renders one NewMessageInput if state.user and state.historyFetched', () => {
+    const wrapped = wrap(<App {...DEFAULT_PROPS} />);
+    expect(wrapped.find(NewMessageInput).length).toEqual(0);
+    wrapped.setState({ user: Usernames.NAS, historyFetched: true });
+    expect(wrapped.find(NewMessageInput).length).toEqual(1);
   });
 
   xit('properly updates state.currentMessage when NewMessageInput is altered', () => {
-    return;
+    const wrapped = wrap(<App {...DEFAULT_PROPS} />);
+    expect(wrapped.state('currentText')).toEqual('');
+    const input = wrapped.children('input');
+    const event = {
+      target: {
+        value: 'Test'
+      }
+    };
+    input.simulate('change', event);
+    expect(wrapped.state('currentText')).toEqual(event.target.value);
   });
 
   xit('resets state.currentText to empty string when message is sent', () => {
-    return;
+    const wrapped = wrapAndSetState(<App {...DEFAULT_PROPS} />, { currentText: 'Test' });
+    const sendButton = wrapped.find(NewMessageInput).children('button');
+    sendButton.simulate('click');
+    expect(wrapped.state('currentText')).toEqual('');
   });
 });
