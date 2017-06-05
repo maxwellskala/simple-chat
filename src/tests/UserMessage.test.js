464e5dd0ef10b0fd2590ae70cad5127c370ac205
Max Skala <max.skala@odysys.com>
2017-06-05 18:13:33 -0400

    Add test for UserMessage

--- src/tests/UserMessage.test.js
+++ src/tests/UserMessage.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import UserMessage from '../components/UserMessage';
 import { wrap } from './utils';
+import * as MessagePositions from '../constants/MessagePositions';
+
+const text = 'Test';
+const DEFAULT_PROPS = {
+  text,
+  side: MessagePositions.LEFT
+};
+const style = { textAlign: MessagePositions.LEFT };
 
 describe('<UserMessage />', () => {
-  xit('uses props.side to set style appropriately', () => {
-    return;
+  it('uses props.side to set style appropriately', () => {
+    const wrapped = wrap(<UserMessage {...DEFAULT_PROPS} />);
+    expect(wrapped.prop('style')).toEqual(style);
   });
 });
