e6e7729ec32f6c8ea78322a30ac3b566ec49140f
Max Skala <max.skala@odysys.com>
2017-06-05 17:10:19 -0400

    Finish stubbing out tests and storing in the appropriate directories

---
+++ src/tests/NewMessageInput.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { wrap } from './utils';
+
+describe('<NewMessageInput />', () => {
+  xit('renders props.currentText to input value', () => {
+    return;
+  });
+});
---
+++ src/tests/UserMessage.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { wrap } from './utils';
+
+describe('<UserMessage />', () => {
+  xit('uses props.side to set style appropriately', () => {
+    return;
+  });
+});
