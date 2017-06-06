a52ebe101ff129e73f0dd8c956223082a5959ce3
Max Skala <max.skala@odysys.com>
2017-06-06 13:00:35 -0400

    Wrap root component in Provider in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PubNub from 'pubnub';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 // dev-specific hot loading stuff
 import 'react-hot-loader/patch';
 import { AppContainer } from 'react-hot-loader';
 
 import App from './containers/App';
+import reducer from './reducers';
 import { PUBNUB_PUBLISH, PUBNUB_SUBSCRIBE } from './ApiKeys';
 
 const pubNub = new PubNub({
@@ -14,10 +17,14 @@ const pubNub = new PubNub({
   ssl: true
 });
 
+const store = createStore(reducer);
+
 const render = Component => {
   ReactDOM.render(
     <AppContainer>
-      <Component pubNub={pubNub} />
+      <Provider store={store}>
+        <Component pubNub={pubNub} />
+      </Provider>
     </AppContainer>,
     document.getElementById('root')
   );
